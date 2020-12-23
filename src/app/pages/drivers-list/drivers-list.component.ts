77f529dfa847b86d3ba57a7da88f4d5831142547
Administrator <lari.farshad@gmail.com>
2020-12-23 22:47:32 +0330

    update

--- src/app/pages/drivers-list/drivers-list.component.ts
+++ src/app/pages/drivers-list/drivers-list.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {DriversList} from './drivers-list.interface';
-import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-drivers-list',
